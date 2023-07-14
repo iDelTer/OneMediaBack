c21b55c4f3ad23237c150b0fd939753942d4319a
iDelTer <ivandelter@outlook.com>
2023-07-14 19:46:30 +0200

    Trying things

--- routes/api.php
+++ routes/api.php
@@ -3,27 +3,25 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
 
-Route::prefix('api')->group(function () {
-    Route::post('/login', function () {
-        return response()->json(['mensaje' => '¡Hola, mundo!'], 200);
-    });
-    Route::post('/account', function (Request $request) {
-        $sessionToken = $request->input('sessionId');
-        return response()->json(['mensaje' => "Tu id es: $sessionToken"]);
-    });
+// Route::prefix('api')->group(function () {
+//     Route::post('/', function () {
+//         return response()->json(['mensaje' => '¡API!'], 200);
+//     });
+//     Route::post('/login', function () {
+//         return response()->json(['mensaje' => '¡Hola, mundo!'], 200);
+//     });
+//     Route::post('/account', function (Request $request) {
+//         $sessionToken = $request->input('sessionId');
+//         return response()->json(['mensaje' => "Tu id es: $sessionToken"]);
+//     });
+// });
+
+Route::get('api', function () {
+    return response()
+        ->header('Content-Type', 'text/json')
+        ->json(['mensaje' => '¡API!'], 200);
 });
